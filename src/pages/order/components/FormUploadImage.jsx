f21abb8a3c2428ba42bfb100a4830890e822762b
nirwanaz <alupna13@gmail.com>
2022-07-29 20:01:27 +0700

    form upload

--- src/pages/order/components/FormUploadImage.jsx
+++ src/pages/order/components/FormUploadImage.jsx
@@ -18,45 +18,46 @@ const FormUploadImage = () => {
     })
   }
 
+  const unSelectFile = () => {
+    setFile({
+      current: undefined,
+      preview: undefined
+    })
+  }
+
   return (
     <div className="image-upload">
-      <label htmlFor="btn-upload">
-        <input
-          type="file"
-          id="btn-upload"
-          style={{ display: 'none' }}
-          accept="image/*"
-          onChange={selectFile}
-        />
-        <Button variant="outlined" color="primary" component="span">
-          Choose Image
-        </Button>
-      </label>
-      <div className="file-name">
-        {file.current ? file.current.name : null}
-      </div>
-      <Button
-        variant="contained"
-        color="primary"
-        component="span"
-        disabled={!file.current}
-        onClick={() => console.log('upload')}
-      >
-        Upload  
-      </Button>
-      {file.current && (
-        <Box className="my20" display="flex" alignItems="center">
-          <Box width="100%" mr={1}>
-
-          </Box>
+      {file.preview ? (
+        <Box display="flex" alignItems="center" flexDirection="column">
+          <Avatar
+            alt="preview-img"
+            src={file.preview}
+            sx={{ width: 240, height: 240}}
+          />
+          <Button variant="outlined" color="primary" component="span" onClick={unSelectFile}>
+            Remove Image
+          </Button>
         </Box>
-      )}
-      {file.preview && (
-        <Avatar
-          alt="preview-img"
-          src={file.preview}
-          sx={{ width: 100, height: 100}}
-        />
+      ) : (
+        <Box
+          component="label"
+        >
+          <input
+            type="file"
+            id="btn-upload"
+            style={{ display: 'none' }}
+            accept="image/*"
+            onChange={selectFile}
+          />
+          <Button
+            variant="outlined"
+            color="primary"
+            component="span"
+            sx={{ borderRadius: "50%", width: 240, height: 240 }}
+          >
+            Choose Image
+          </Button>
+        </Box>
       )}
     </div>
   )
