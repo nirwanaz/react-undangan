071f8693d062eb3bd87f40305d8ebdfab349b63f
nirwanaz <alupna13@gmail.com>
2022-07-29 18:36:10 +0700

    form upload image

--- src/pages/order/components/FormWeddingCouple.jsx
+++ src/pages/order/components/FormWeddingCouple.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { useState } from "react";
 import Grid from '@mui/material/Grid';
-import { Box, Button, FormControl, FormGroup, FormHelperText, FormLabel, OutlinedInput, TextField } from "@mui/material";
+import { Box, Button, FormControl, FormLabel, TextField } from "@mui/material";
+import FormUploadImage from "./FormUploadImage";
+
 
 const FormWeddingCouple = ({ nextStep, handleFormData, values }) => {
   const [error, setError] = useState(false)
   
   const submitFormData = (e) => {
     e.preventDefault()
-    setError(false)
 
-    if (!values.name) setError(true)
+    if (!values.name ||
+        !values.fatherName ||
+        !values.motherName
+      ) 
+    {
+      setError(true)
+    } else {
+      setError(false)
+      nextStep()
+    }
   }
 
   return (
@@ -24,39 +34,34 @@ const FormWeddingCouple = ({ nextStep, handleFormData, values }) => {
     >
       <FormControl component="fieldset">
         <FormLabel component="legend">Form Wedding Couple</FormLabel>
-        <FormGroup>
-          <TextField
-            name="myname"
-            label="Nama pria"
-            value={values.name}
-            onChange={handleFormData("name")}
-            error={error}
-            margin="normal"
-          />
-          {error && <FormHelperText>This is a required field</FormHelperText>}
-        </FormGroup>
-        <FormGroup>
-          <TextField
-            name="father"
-            label="Nama Bapak"
-            value={values.fatherName}
-            onChange={handleFormData("fatherName")}
-            error={error}
-            margin="normal"
-          />
-          {error && <FormHelperText>This is a required field</FormHelperText>}
-        </FormGroup>
-        <FormGroup>
-          <TextField
-            name="mother"
-            label="Nama Ibu"
-            value={values.motherName}
-            onChange={handleFormData("motherName")}
-            error={error}
-            margin="normal"
-          />
-          {error && <FormHelperText>This is a required field</FormHelperText>}
-        </FormGroup>
+        <FormUploadImage />
+        <TextField
+          name="myname"
+          label="Nama pria"
+          value={values.name}
+          onChange={handleFormData("name")}
+          error={error}
+          helperText={error && 'this is a required field'}
+          margin="normal"
+        />
+        <TextField
+          name="father"
+          label="Nama Bapak"
+          value={values.fatherName}
+          onChange={handleFormData("fatherName")}
+          error={error}
+          helperText={error && 'this is a required field'}
+          margin="normal"
+        />
+        <TextField
+          name="mother"
+          label="Nama Ibu"
+          value={values.motherName}
+          onChange={handleFormData("motherName")}
+          error={error}
+          helperText={error && 'this is a required field'}
+          margin="normal"
+        />
         <Button
           variant="contained"
           color="primary"
